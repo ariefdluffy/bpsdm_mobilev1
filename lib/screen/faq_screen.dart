682a8aa961f7a100d30822a82f8b891f5cd34862
arieftheluffy <arieftheluffy@gmail.com>
2025-02-19 21:34:33 +0800

    bug-layout

--- lib/screen/faq_screen.dart
+++ lib/screen/faq_screen.dart
@@ -2,7 +2,6 @@ import 'package:bpsdm_mobilev1/providers/api_provider.dart';
 import 'package:bpsdm_mobilev1/screen/widget/shimmer_box.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
-// import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FaqScreen extends ConsumerWidget {
@@ -22,94 +21,110 @@ class FaqScreen extends ConsumerWidget {
           backgroundColor: Colors.white,
           foregroundColor: Colors.black,
         ),
-        body: Column(
-          children: [
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
-              child: TextField(
-                controller: textController,
-                onChanged: (searchQuery) =>
-                    ref.read(searchQueryProvider.notifier).state = searchQuery,
-                decoration: InputDecoration(
-                  labelText: "Cari Pertanyaan...",
-                  prefixIcon: const Icon(Icons.search),
-                  suffixIcon: searchQuery.isNotEmpty
-                      ? IconButton(
-                          icon: const Icon(Icons.clear),
-                          onPressed: () {
-                            textController.clear();
-                            FocusScope.of(context).unfocus();
-                            ref.read(searchQueryProvider.notifier).state = "";
-                          })
-                      : null,
-                  border: const OutlineInputBorder(),
-                ),
-              ),
-            ),
-            Expanded(
-              child: faqAsyncValue.when(
-                data: (faqs) {
-                  final filteredFAQ = faqs.where((faq) {
-                    return faq.question
-                        .toLowerCase()
-                        .contains(searchQuery.toLowerCase());
-                  }).toList();
-
-                  return filteredFAQ.isEmpty
-                      ? const Center(child: Text("Tidak ada hasil"))
-                      : ListView.builder(
-                          itemCount: filteredFAQ.length,
-                          itemBuilder: (context, index) {
-                            String contentAnswer = filteredFAQ[index].answer;
-                            bool containsHtml = contentAnswer.contains("<") &&
-                                contentAnswer.contains(">");
-                            return Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 8.0, vertical: 4),
-                              child: Card(
-                                elevation: 4,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                                child: ExpansionTile(
-                                  title: Text(
-                                    filteredFAQ[index].question,
-                                    style: const TextStyle(
-                                        fontWeight: FontWeight.w500),
-                                  ),
-                                  leading: const Icon(Icons.question_answer,
-                                      color: Colors.blueAccent),
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                          horizontal: 16.0, vertical: 8),
-                                      // child:
-                                      // Html(data: filteredFAQ[index].answer),
-                                      child: containsHtml
-                                          ? Html(data: contentAnswer)
-                                          : Text(
-                                              contentAnswer,
-                                              textAlign: TextAlign.justify,
-                                            ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                            );
-                          },
-                        );
-                },
-                loading: () => Column(
-                  children: List.generate(
-                    8,
-                    (index) => const ShimmerBox(),
+        body: Center(
+          child: LayoutBuilder(builder: (context, constraints) {
+            double maxWidth =
+                constraints.maxWidth > 600 ? 600 : constraints.maxWidth;
+            return SizedBox(
+              width: maxWidth,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16.0, vertical: 8),
+                    child: TextField(
+                      controller: textController,
+                      onChanged: (searchQuery) => ref
+                          .read(searchQueryProvider.notifier)
+                          .state = searchQuery,
+                      decoration: InputDecoration(
+                        labelText: "Cari Pertanyaan...",
+                        prefixIcon: const Icon(Icons.search),
+                        suffixIcon: searchQuery.isNotEmpty
+                            ? IconButton(
+                                icon: const Icon(Icons.clear),
+                                onPressed: () {
+                                  textController.clear();
+                                  FocusScope.of(context).unfocus();
+                                  ref.read(searchQueryProvider.notifier).state =
+                                      "";
+                                })
+                            : null,
+                        border: const OutlineInputBorder(),
+                      ),
+                    ),
                   ),
-                ),
-                error: (error, stack) => Center(child: Text('Error: $error')),
+                  Expanded(
+                    child: faqAsyncValue.when(
+                      data: (faqs) {
+                        final filteredFAQ = faqs.where((faq) {
+                          return faq.question
+                              .toLowerCase()
+                              .contains(searchQuery.toLowerCase());
+                        }).toList();
+
+                        return filteredFAQ.isEmpty
+                            ? const Center(child: Text("Tidak ada hasil"))
+                            : ListView.builder(
+                                itemCount: filteredFAQ.length,
+                                itemBuilder: (context, index) {
+                                  String contentAnswer =
+                                      filteredFAQ[index].answer;
+                                  bool containsHtml =
+                                      contentAnswer.contains("<") &&
+                                          contentAnswer.contains(">");
+                                  return Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 8.0, vertical: 4),
+                                    child: Card(
+                                      elevation: 4,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      child: ExpansionTile(
+                                        title: Text(
+                                          filteredFAQ[index].question,
+                                          style: const TextStyle(
+                                              fontWeight: FontWeight.w500),
+                                        ),
+                                        leading: const Icon(
+                                            Icons.question_answer,
+                                            color: Colors.blueAccent),
+                                        children: [
+                                          Padding(
+                                            padding: const EdgeInsets.symmetric(
+                                                horizontal: 16.0, vertical: 8),
+                                            // child:
+                                            // Html(data: filteredFAQ[index].answer),
+                                            child: containsHtml
+                                                ? Html(data: contentAnswer)
+                                                : Text(
+                                                    contentAnswer,
+                                                    textAlign:
+                                                        TextAlign.justify,
+                                                  ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                },
+                              );
+                      },
+                      loading: () => Column(
+                        children: List.generate(
+                          8,
+                          (index) => const ShimmerBox(),
+                        ),
+                      ),
+                      error: (error, stack) =>
+                          Center(child: Text('Error: $error')),
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ],
+            );
+          }),
         ),
       ),
     );
